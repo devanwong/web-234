be3fa5c0a5008a1dec0b1f6fa4469964d4d3e457
Devan Wong <devan.wong@yahoo.com>
2020-06-26 12:57:32 -0700

    Update barleta-findErrors.js

--- week-5/barleta-findErrors.js
+++ week-5/barleta-findErrors.js
@@ -1,46 +1,38 @@
 /*
-============================================
-; Title:  barleta-findErrors.js
-; Author: Marie Nicole Barleta
-; Date:   25 June 2020
-; Modified By:
-; Description: Find errors advanced arrays
-;===========================================
-*/
-
-// import statement
-var header = require('../barleta-header.js');
-console.log(header.display("Marie", "Barleta", "Find errors advanced arrays"));
-console.log("\n")
-
+ 	============================================
+ 	; Title: barleta-findErrors.js
+ 	; Author: Marie Nicole Barleta
+ 	; Date: 25 June 2020
+ 	; Modified By: Devan Wong
+ 	; Description: Find errors advanced arrays
+ 	;===========================================
+   */
+   
 /*Expected output:
-Do
-Re
-Mi
-Fa
-So
-La
-Ti
-Do
-*/
+ 	Do
+ 	Re
+ 	Mi
+ 	Fa
+ 	So
+ 	La
+ 	Ti
+ 	Do
+ 	*/
 
-//Code has multiple errors
-let chords = new Map();
+ 	//Code has multiple errors
+ 	let chords = new Map();
+// added lots of ';' to make code more legible. 
+ 	chords.set('C', 'Do' );
+ 	chords.set('D', 'Re' );
+ 	chords.set('E', 'Mi' );
+ 	chords.set('F', 'Fa' );
+ 	chords.set('G', 'So' );
+ 	chords.set('A', 'La' );
+ 	chords.set('B', 'Ti' );
 
-chords.set('C', 'Do' )
-chords.set('D', 'Re' )
-chords.set('E', 'Mi' )
-chords.set('F', 'Fa' )
-chords.set('G', 'So' )
-chords.set('A', 'La' )
-chords.set('B', 'Ti' )
-
-
-for (let [key, value] of chords){
-  console.log(value);
-  console.log(key);
-  console.log(key, value)
-}
-
-console.log(chords.get('C',key));
-console.log(chords.get('Do'))
+  //deletes all the console.log so we can see a clean output.
+ 	for (let [key, value] of chords){
+    console.log(value);
+   }
+  //deleted "key" and kept it C to grab the value 'Do'.
+  console.log(chords.get('C'));
